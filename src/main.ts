975bfb3097f2b31efdfd2ce4b02595362a6c55ad
Mateus Sombrio <mateussombrio@ssp.sc.gov.br>
2026-05-20 09:55:56 -0300

    chore: criando o .env

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import "reflect-metadata";
 import { bancoDados } from "./dbConfig.js";
 import { App } from "./app.js";
 
-const app = new App(3000, bancoDados);
+const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+const app = new App(port, bancoDados);
 app.start();
